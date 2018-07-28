6929b5f01e107636556390070b3a6e5cb9b04050
feilicsuzan <felicsuzan_98@163.com>
2018-07-28 14:20:21 +0800

    submit ex13.py

---
+++ ex13.py
@@ -0,0 +1,14 @@
+x=1
+y=2
+z=3
+a='a'
+b='b'
+c='c'
+sets={x,y,z,a,b,c};
+diction={'x':x,'y':y,'z':z,4:a,5:b,6:c}
+print sets 
+print diction
+print diction[x]
+print sets[1]
+diction['extra']=5
+print diction
