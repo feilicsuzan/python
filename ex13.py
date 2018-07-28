70eb6b84ace7e1e3e0f265e176d4dbd0e1ba5a97
feilicsuzan <felicsuzan_98@163.com>
2018-07-28 14:42:30 +0800

    submit ex13.py

--- ex13.py
+++ ex13.py
@@ -1,4 +1,5 @@
-x=1
+#number 1
+'''x=1
 y=2
 z=3
 a='a'
@@ -11,4 +12,25 @@ print diction
 print diction[x]
 print sets[1]
 diction['extra']=5
-print diction
+print diction'''
+#number 2
+'''cats=20
+dogs=30
+if cats<dogs:
+    print"More dogs than the cats."
+elif dogs<cats:
+    print"More cats than dogs."
+else:
+    print"Same number of cats and dogs."'''
+#number 3
+def circumference(r):
+    pi=3.14
+    return pi*r*2
+from sys import argv;#get three string from terminate
+script,c=argv;
+if(c=='earth'):
+    print"the circumference of earth is",circumference(6078000)
+elif(c=='mars'):
+    print"the circumference of mars is",circumference(3368000)
+else:
+    print"please put in correct choose"
